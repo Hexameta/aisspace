6df234a7d31a3b4257fba84a46a35385326a8771
Jino <jinomanoj3@gmail.com>
2023-09-06 12:23:07 +0530

    admin page design

--- backend/admin/news.php
+++ backend/admin/news.php
@@ -93,6 +93,36 @@
                         <span class="nav-link-text ms-1">News Feed</span>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link  " href="../admin/main-page.php">
+                        <div
+                            class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <svg width="12px" height="12px" viewBox="0 0 40 40" version="1.1"
+                                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <title>settings</title>
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <g transform="translate(-2020.000000, -442.000000)" fill="#FFFFFF"
+                                        fill-rule="nonzero">
+                                        <g transform="translate(1716.000000, 291.000000)">
+                                            <g transform="translate(304.000000, 151.000000)">
+                                                <polygon class="color-background opacity-6"
+                                                    points="18.0883333 15.7316667 11.1783333 8.82166667 13.3333333 6.66666667 6.66666667 0 0 6.66666667 6.66666667 13.3333333 8.82166667 11.1783333 15.315 17.6716667">
+                                                </polygon>
+                                                <path class="color-background opacity-6"
+                                                    d="M31.5666667,23.2333333 C31.0516667,23.2933333 30.53,23.3333333 30,23.3333333 C29.4916667,23.3333333 28.9866667,23.3033333 28.48,23.245 L22.4116667,30.7433333 L29.9416667,38.2733333 C32.2433333,40.575 35.9733333,40.575 38.275,38.2733333 L38.275,38.2733333 C40.5766667,35.9716667 40.5766667,32.2416667 38.275,29.94 L31.5666667,23.2333333 Z">
+                                                </path>
+                                                <path class="color-background"
+                                                    d="M33.785,11.285 L28.715,6.215 L34.0616667,0.868333333 C32.82,0.315 31.4483333,0 30,0 C24.4766667,0 20,4.47666667 20,10 C20,10.99 20.1483333,11.9433333 20.4166667,12.8466667 L2.435,27.3966667 C0.95,28.7083333 0.0633333333,30.595 0.00333333333,32.5733333 C-0.0583333333,34.5533333 0.71,36.4916667 2.11,37.89 C3.47,39.2516667 5.27833333,40 7.20166667,40 C9.26666667,40 11.2366667,39.1133333 12.6033333,37.565 L27.1533333,19.5833333 C28.0566667,19.8516667 29.01,20 30,20 C35.5233333,20 40,15.5233333 40,10 C40,8.55166667 39.685,7.18 39.1316667,5.93666667 L33.785,11.285 Z">
+                                                </path>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+                        <span class="nav-link-text ms-1">Main Page</span>
+                    </a>
+                </li>
             </ul>
         </div>
     </aside>
@@ -129,86 +159,24 @@
         </nav>
         <!-- End Navbar -->
         <div class="container-fluid py-4">
-            <!-- <div class="row">
-                <div class="col-12">
-                    <div class="card mb-4">
-                        <div class="card-header pb-0">
-                            <div
-                                class="d-flex bg-gradient-primary text justify-content-between border-radius-lg pt-3 p-3">
-                                <h6 class="text-white font-weight-bolder text-uppercase pt-2 ps-3">News Feed</h6>
-
-                                <button class="btn bg-gradient-dark m-0 toast-btn" type="button" data-toggle="modal"
-                                    data-target="#form"><i class="fas fa-plus"></i>&nbsp;&nbsp;Add New Item</button>
-                            </div>
-
-                        </div>
-                        <div class="card-body px-0 pb-2">
-                            <div class="table-responsive p-0">
-                                <table class="align-items-center mb-0 w-100 d-block d-md-table">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
-                                                style="width: 20%;">
-                                                Title</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
-                                                style="width: 10%;">
-                                                Date</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 th-sm"
-                                                style="width: 50%;">
-                                                Description</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
-                                                style="width: 10%;">
-                                                Image</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
-                                                style="width: 10%;">
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td class="align-middle text-center">
-                                                <h6 class="mb-0 text-xs font-weight-bolder mx-3">
-                                                Entrepreneurial Exibition Stalls - Part of Incubation Inaugration
-                                                </h6>
-                                            </td>
-                                            <td class="align-middle text-center">
-                                                <span class="text-sm font-weight-bolder">12/10/2021</span>
-                                            </td>
-                                            <td class="align-middle text-sm">
-                                                <p class="text-xs font-weight-bold mb-0 p-4">
-                                                    Hexameta Technologies is a software development company that
-                                                    provides end-to-end development service for web and mobile
-                                                    development. We work on a wide range of technologies ranging from
-                                                    open source to proprietary and custom built solutions.
-                                                </p>
-                                            </td>
-                                            <td class="align-middle text-center text-sm">
-                                                <img src="../../assets/images/news/news1.png" alt=""
-                                                    height="100" width="100" style="object-fit: contain;"
-                                                    data-toggle="modal" data-target="#view">
-                                            </td>
-                                            <td class="align-middle text-center">
-                                                <a class="btn btn-link text-danger text-gradient px-3 mb-0">
-                                                    <i class="far fa-trash-alt me-2"></i>Delete
-                                                </a>
-                                                <br>
-                                                <a class="btn btn-link text-dark px-3 mb-0" data-toggle="modal"
-                                                    data-target="#edit"><i class="fas fa-pencil-alt text-dark me-2"
-                                                        aria-hidden="true"></i>Edit
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
             <div class="col-12 mt-4">
                 <div class="card mb-4">
+                    <div class="d-flex bg-gradient-primary text justify-content-between border-radius-lg pt-3 p-3">
+                        <h6 class="text-white font-weight-bolder text-uppercase pt-2 ps-3">News Feed</h6>
+                    </div>
                     <div class="card-body p-3">
                         <div class="row">
+                            
+                        <div class="col-xl-3 col-md-6 mb-xl-0 mb-4">
+                                <div class="card h-100 card-plain border">
+                                    <div class="card-body d-flex flex-column justify-content-center text-center">
+                                        <a type="button" title="Add News Item" data-toggle="modal" data-target="#form">
+                                            <i class="fa fa-plus text-secondary mb-3"  ></i>
+                                            <h5 class=" text-secondary"> Add News Item </h5>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="col-xl-3 col-md-6 mb-xl-0 mb-4">
                                 <div class="card card-blog card-plain">
                                     <div class="position-relative">
@@ -223,11 +191,9 @@
                                             <i class="fas fa-calendar-alt text-dark me-2" aria-hidden="true"></i>
                                             <p class="text-gradient text-dark mb-2 text-sm">12/10/2021</p>
                                         </div>
-                                        <a href="javascript:;">
                                             <h4 class="text-lg font-weight-bolder">
                                                 Entrepreneurial Exibition Stalls - Part of Incubation Inaugration
                                             </h4>
-                                        </a>
                                         <p class="text-sm mb-4">
                                             Hexameta Technologies is a software development company that
                                             provides end-to-end development service for web and mobile
@@ -235,21 +201,11 @@
                                             open source to proprietary and custom built solutions.
                                         </p>
                                         <div class="d-flex align-items-center justify-content-between">
-                                            <button type="button" class="btn btn-outline-primary btn-sm mb-0">Delete
+                                            <button type="button" title="Delete this News" class="btn btn-outline-primary btn-sm mb-0">Delete
                                                 News</button>
-                                            <buttone type="button" class="btn btn-primary btn-sm mb-0"
+                                            <buttone type="button" title="Edit this News" class="btn btn-primary btn-sm mb-0"
                                                 data-toggle="modal" data-target="#edit">Edit News</button>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6 mb-xl-0 mb-4">
-                                <div class="card h-100 card-plain border">
-                                    <div class="card-body d-flex flex-column justify-content-center text-center">
-                                        <a type="button" data-toggle="modal" data-target="#form">
-                                            <i class="fa fa-plus text-secondary mb-3"></i>
-                                            <h5 class=" text-secondary"> Add News Item </h5>
-                                        </a>
                                     </div>
                                 </div>
                             </div>
